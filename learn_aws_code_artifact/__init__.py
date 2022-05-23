8a70d050c83fb8ab9c6f96c31cb19e6f4975caca
Sanhe <MacHu-GWU@users.noreply.github.com>
2022-05-23 10:44:01 -0400

    update readme

--- learn_aws_code_artifact/__init__.py
+++ learn_aws_code_artifact/__init__.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 
 """
-Package Description.
+A dummy package for learning AWS CodeArtifact.
 """
 
 
 from ._version import __version__
 
-__short_description__ = "Package short description."
+__short_description__ = "A dummy package for learning AWS CodeArtifact."
 __license__ = "MIT"
 __author__ = "Sanhe Hu"
 __author_email__ = "husanhe@gmail.com"
 __github_username__ = "MacHu-GWU"
 
-print("64d771494cf79901503819695b30ea8c")
+# you can use this to verify
+print("The content of this release is: 64d771494cf79901503819695b30ea8c")
